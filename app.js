7ab8767f660e974476a54d4d91bd164df77a257c
ssportplus <ridvan.akpunar@ssportplus.com>
2021-03-28 02:13:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -303,6 +303,7 @@ function titleCase(string) {
     for (let i = 0; i < sentence.length; i++) {
         sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
     }
+    return sentence;
 };
 const radioChoice = () => {
     if (codeRadio.checked) {
